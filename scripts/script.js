8c93874d4b3937bbe1b395c808660ad0eea9d5ab
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-02-28 16:08:49 +0100

    Esercizio completato.
    Snack 2.3 Fatto in modo semplice
    
    Adesso faccio in modo più funzionale.

--- scripts/script.js
+++ scripts/script.js
@@ -55,10 +55,11 @@
 
 
 
-// Snack 1.3
+// // Snack 1.3
 
-// Il software deve chiedere per 10 volte all’utente di inserire un numero.
-// Il programma stampa la somma di tutti i numeri inseriti.
+// // Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// // Il programma stampa la somma di tutti i numeri inseriti.
+
 // let netTotal = 0;
 // let total = 0; 
 // let output = document.querySelector('#target2');
@@ -78,11 +79,11 @@
 
 
 // output.innerHTML = `Numero totale è: ${netTotal}`;
-// // console.log(netTotal);
+// console.log(netTotal);
 
 
 
-//Snack 1.4
+// //Snack 1.4
 
 
 
@@ -109,7 +110,7 @@
 // let found = false;
 
 // for (let i = 0; i < emailList.length; i++) {
-//     if (emailList[i].toLowerCase == userName.toLowerCase) {
+//     if (emailList[i].toLowerCase() == userName.toLowerCase()) {
 //         found = true;
 //     }
 // }
@@ -134,6 +135,7 @@
 
 
 // //Snack 1.5
+
 // const stampa = document.querySelector(".target5");
 // let arrVuotoDispari = [];
 // let arrVuotoPari = [];
@@ -150,33 +152,121 @@
 
 // stampa.innerHTML = ` Array dispari:${arrVuotoDispari} <br><br> Array Pari:${arrVuotoPari}`;
 
-//Snack 1.6
+// // Snack 1.6
 
- let inputNumber = document.querySelector('#user-number');
- let btnSend2 = document.querySelector('#send2')
- let output = document.querySelector('.target6')
+//  let inputNumber = document.querySelector('#user-number');
+//  const btnSend2 = document.querySelector('#send2')
+//  let output = document.querySelector('.target6')
 
-btnSend2.addEventListener('click', function() {
+// btnSend2.addEventListener('click', function() {
 
-    let userNumber = inputNumber.value;
-    let totalNumber = [];
-    if (userNumber < 1000) {
-        output.innerHTML = "It must be 4 digit number";
-    } else if (userNumber != parseInt(userNumber)) {
-        output.innerHTML = "You must enter number";
-    } else {
-        for (let index = 0; index < userNumber.length; index++) {
-                totalNumber.push(userNumber[index]);
-                console.log(totalNumber);
+//     let userNumber = inputNumber.value;
+//     let totalNumber = [];
+//     if (userNumber.length != 4) {
+//         output.innerHTML = "It must be 4 digit number";
+//     }
+//     else if (userNumber != parseInt(userNumber)) {
+//         output.innerHTML = "You must enter number";
+//     } else {
+//         for (let index = 0; index < userNumber.length; index++) {
+//                 totalNumber.push(userNumber[index]);
+//                 console.log(totalNumber);
        
         
-    }
-        let sumTotal = 0;
-        for (let i = 0; i < totalNumber.length; i++) {
-        sumTotal += parseInt(totalNumber[i]);
-        }
-        output.innerHTML = sumTotal;
-    }
+//     }
+//         let sumTotal = 0;
+//         for (let i = 0; i < totalNumber.length; i++) {
+//         sumTotal += parseInt(totalNumber[i]);
+//         }
+//         output.innerHTML = sumTotal;
+//     }
    
     
-})
+// })
+
+
+/* //Snack 2.0
+const arr = [];
+
+let sum = 0;
+let media = 0;
+
+for (let index = 1; index <= 10; index++) {
+    arr.push(index);
+}
+
+let index = 0;
+while (index < arr.length) {
+    sum += arr[index];
+    index++;
+}
+
+media = sum / arr.length;
+
+console.log(`totale: ${sum}`);
+console.log(`media: ${media}`);
+
+*/
+
+// // Snack 2.1 
+
+// const arr = [];
+
+// let sumFor = 0;
+// let sumWhile = 0;
+// let index = 0;
+// while ( index < 5) {
+
+//     arr.push(parseInt(prompt('Inserisci numero')));
+//     index++
+// }
+
+// let index2 = 0;
+// while (index2 < arr.length) {
+//     sumWhile += arr[index2];
+//     index2++;
+    
+// }
+
+// for (let index = 0; index < arr.length; index++ ) {
+//     sumFor += arr[index];
+// }
+
+// console.log(`totale usando For: ${sumFor}`);
+// console.log(`totale usando While: ${sumWhile}`);
+
+
+// // Snack 2.2 
+
+// let numero = parseInt(prompt('inserisci un numero'));
+
+// if (numero % 2 == 0) {
+//     console.log(numero);
+// } else {
+//     numero += 1;
+//     console.log(numero);
+// }
+
+//Snack 2.3 
+
+const arrName = ['Bruce', 'Tom' , 'Metal'];
+const arrSurname = ['Hardy', 'Rock' , 'Lee'];
+const arrNameSurname = [];
+
+let listCount = 1;
+let RandomNameSurname;
+while (listCount <= 3) {
+    let counterName = randomNum(0, 3);
+    let counterSurname = randomNum(0, 3);
+    console.log(counterName);
+    console.log(counterSurname);
+    RandomNameSurname = arrName[counterName] + " " + arrSurname[counterSurname];
+    arrNameSurname.push(RandomNameSurname);
+    listCount++;
+}
+
+console.log(`Random Generated List: ${arrNameSurname}`);
+
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
